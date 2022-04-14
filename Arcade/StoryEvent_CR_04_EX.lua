ace93d8ab3b2d3fe6ebf4411538ed4a73dc6c777
Jordon87 <38020644+Jordon87@users.noreply.github.com>
2022-04-14 12:34:57 +1200

    Add files via upload

--- Arcade/StoryEvent_CR_04_EX.lua
+++ Arcade/StoryEvent_CR_04_EX.lua
@@ -1,50 +1,23 @@
-local L0_1, L1_1
-L0_1 = {}
-function L1_1()
-  local L0_2, L1_2
-  L0_2 = ReturnToArcadeMenu
-  L0_2()
-end
-L0_1.onQuit = L1_1
-function L1_1()
-  local L0_2, L1_2
-  L0_2 = ReturnToArcadeMenu
-  L0_2()
-end
-L0_1.onContinue = L1_1
-function L1_1()
-  local L0_2, L1_2
-  L0_2 = DebugPrint
-  L1_2 = "onUnlocked()"
-  L0_2(L1_2)
-end
-L0_1.onUnlocked = L1_1
-function L1_1()
-  local L0_2, L1_2
-  L0_2 = DebugPrint
-  L1_2 = "onFailed()"
-  L0_2(L1_2)
-end
-L0_1.onFailed = L1_1
-function L1_1()
-  local L0_2, L1_2
-  L0_2 = DebugPrint
-  L1_2 = "onPlaced()"
-  L0_2(L1_2)
-end
-L0_1.onPlaced = L1_1
-function L1_1()
-  local L0_2, L1_2
-  L0_2 = DebugPrint
-  L1_2 = "onFirst()"
-  L0_2(L1_2)
-end
-L0_1.onFirst = L1_1
-function L1_1()
-  local L0_2, L1_2
-  L0_2 = DebugPrint
-  L1_2 = "onCutsceneFinished()"
-  L0_2(L1_2)
-end
-L0_1.onCutsceneFinished = L1_1
-StoryEvent_CR_04_EX = L0_1
+StoryEvent_CR_04_EX = {
+  onQuit = function()
+    ReturnToArcadeMenu()
+  end,
+  onContinue = function()
+    ReturnToArcadeMenu()
+  end,
+  onUnlocked = function()
+    DebugPrint("onUnlocked()")
+  end,
+  onFailed = function()
+    DebugPrint("onFailed()")
+  end,
+  onPlaced = function()
+    DebugPrint("onPlaced()")
+  end,
+  onFirst = function()
+    DebugPrint("onFirst()")
+  end,
+  onCutsceneFinished = function()
+    DebugPrint("onCutsceneFinished()")
+  end
+}
--- Arcade/VehiclePickups.lua
+++ Arcade/VehiclePickups.lua
@@ -1,184 +1,44 @@
-local L0_1, L1_1
-L0_1 = {}
-function L1_1(A0_2)
-  local L1_2, L2_2, L3_2, L4_2, L5_2, L6_2, L7_2, L8_2, L9_2, L10_2, L11_2
-  L1_2 = PlayUISound
-  L2_2 = UISoundEventUnlock_Asset
-  L1_2(L2_2)
-  L1_2 = PushMenu
-  L2_2 = "UnlockedPopup"
-  L1_2(L2_2)
-  L1_2 = IsEventOnCriticalPath
-  L2_2 = "StoryEvent_"
-  L3_2 = A0_2
-  L2_2 = L2_2 .. L3_2
-  L1_2 = L1_2(L2_2)
-  if L1_2 == 1 then
-    L1_2 = UnlockedPopup
-    L2_2 = L1_2
-    L1_2 = L1_2.UnlockedItem
-    L3_2 = "GoldEvent"
-    L4_2 = ""
-    L5_2 = GetLocalizedString
-    L6_2 = "ingameui"
-    L7_2 = "STR_UNLOCK_GOLDEVENT"
-    L5_2 = L5_2(L6_2, L7_2)
-    L6_2 = " "
-    L7_2 = GetLocalizedString
-    L8_2 = "EventInfo"
-    L9_2 = "STR_"
-    L10_2 = A0_2
-    L11_2 = "_TITLE"
-    L9_2 = L9_2 .. L10_2 .. L11_2
-    L7_2 = L7_2(L8_2, L9_2)
-    L4_2 = L4_2 .. L5_2 .. L6_2 .. L7_2
-    L1_2(L2_2, L3_2, L4_2)
-  else
-    L1_2 = UnlockedPopup
-    L2_2 = L1_2
-    L1_2 = L1_2.UnlockedItem
-    L3_2 = "NormalEvent"
-    L4_2 = ""
-    L5_2 = GetLocalizedString
-    L6_2 = "ingameui"
-    L7_2 = "STR_GLOBAL_UNLOCKS_3"
-    L5_2 = L5_2(L6_2, L7_2)
-    L6_2 = " "
-    L7_2 = GetLocalizedString
-    L8_2 = "EventInfo"
-    L9_2 = "STR_"
-    L10_2 = A0_2
-    L11_2 = "_TITLE"
-    L9_2 = L9_2 .. L10_2 .. L11_2
-    L7_2 = L7_2(L8_2, L9_2)
-    L4_2 = L4_2 .. L5_2 .. L6_2 .. L7_2
-    L1_2(L2_2, L3_2, L4_2)
+EventDatabase = {
+  onUnlockedEvent = function(_ARG_0_)
+    PlayUISound(UISoundEventUnlock_Asset)
+    PushMenu("UnlockedPopup")
+    if IsEventOnCriticalPath("StoryEvent_" .. _ARG_0_) == 1 then
+      UnlockedPopup:UnlockedItem("GoldEvent", "" .. GetLocalizedString("ingameui", "STR_UNLOCK_GOLDEVENT") .. " " .. GetLocalizedString("EventInfo", "STR_" .. _ARG_0_ .. "_TITLE"))
+    else
+      UnlockedPopup:UnlockedItem("NormalEvent", "" .. GetLocalizedString("ingameui", "STR_GLOBAL_UNLOCKS_3") .. " " .. GetLocalizedString("EventInfo", "STR_" .. _ARG_0_ .. "_TITLE"))
+    end
+    RaiseMenu("UnlockedPopup")
   end
-  L1_2 = RaiseMenu
-  L2_2 = "UnlockedPopup"
-  L1_2(L2_2)
-end
-L0_1.onUnlockedEvent = L1_1
-EventDatabase = L0_1
-L0_1 = {}
-function L1_1(A0_2)
-  local L1_2, L2_2, L3_2, L4_2, L5_2, L6_2, L7_2, L8_2, L9_2
-  L1_2 = PlayUISound
-  L2_2 = UISoundEventUnlock_Asset
-  L1_2(L2_2)
-  L1_2 = PushMenu
-  L2_2 = "UnlockedPopup"
-  L1_2(L2_2)
-  L1_2 = UnlockedPopup
-  L2_2 = L1_2
-  L1_2 = L1_2.UnlockedItem
-  L3_2 = "Parts"
-  L4_2 = ""
-  L5_2 = GetLocalizedString
-  L6_2 = "ingameui"
-  L7_2 = "STR_GLOBAL_UNLOCKS_5"
-  L5_2 = L5_2(L6_2, L7_2)
-  L6_2 = " "
-  L7_2 = GetLocalizedString
-  L8_2 = "CommonUI"
-  L9_2 = "STR_MCQ_NAME"
-  L7_2 = L7_2(L8_2, L9_2)
-  L4_2 = L4_2 .. L5_2 .. L6_2 .. L7_2
-  L1_2(L2_2, L3_2, L4_2)
-  L1_2 = RaiseMenu
-  L2_2 = "UnlockedPopup"
-  L1_2(L2_2)
-end
-L0_1.onUnlockedPart = L1_1
-CustomParts = L0_1
-L0_1 = {}
-function L1_1(A0_2)
-  local L1_2, L2_2, L3_2, L4_2, L5_2, L6_2, L7_2, L8_2, L9_2
-  L1_2 = PlayUISound
-  L2_2 = UISoundEventUnlock_Asset
-  L1_2(L2_2)
-  L1_2 = PushMenu
-  L2_2 = "UnlockedPopup"
-  L1_2(L2_2)
-  L1_2 = UnlockedPopup
-  L2_2 = L1_2
-  L1_2 = L1_2.UnlockedItem
-  L3_2 = "PaintJobs"
-  L4_2 = ""
-  L5_2 = GetLocalizedString
-  L6_2 = "ingameui"
-  L7_2 = "STR_GLOBAL_UNLOCKS_2"
-  L5_2 = L5_2(L6_2, L7_2)
-  L6_2 = " "
-  L7_2 = GetLocalizedString
-  L8_2 = "CommonUI"
-  L9_2 = A0_2
-  L7_2 = L7_2(L8_2, L9_2)
-  L4_2 = L4_2 .. L5_2 .. L6_2 .. L7_2
-  L1_2(L2_2, L3_2, L4_2)
-  L1_2 = RaiseMenu
-  L2_2 = "UnlockedPopup"
-  L1_2(L2_2)
-end
-L0_1.onCollected = L1_1
-PaintJobs = L0_1
-L0_1 = {}
-function L1_1(A0_2)
-  local L1_2, L2_2, L3_2, L4_2, L5_2, L6_2, L7_2
-  L1_2 = PlayUISound
-  L2_2 = UISoundEventUnlock_Asset
-  L1_2(L2_2)
-  L1_2 = PushMenu
-  L2_2 = "UnlockedPopup"
-  L1_2(L2_2)
-  L1_2 = UnlockedPopup
-  L2_2 = L1_2
-  L1_2 = L1_2.UnlockedItem
-  L3_2 = "WheelSets"
-  L4_2 = ""
-  L5_2 = GetLocalizedString
-  L6_2 = "ingameui"
-  L7_2 = "STR_GLOBAL_UNLOCKS_1"
-  L5_2 = L5_2(L6_2, L7_2)
-  L4_2 = L4_2 .. L5_2
-  L1_2(L2_2, L3_2, L4_2)
-  L1_2 = RaiseMenu
-  L2_2 = "UnlockedPopup"
-  L1_2(L2_2)
-end
-L0_1.onCollected = L1_1
-WheelSets = L0_1
-L0_1 = {}
-function L1_1(A0_2)
-  local L1_2, L2_2, L3_2, L4_2, L5_2, L6_2, L7_2, L8_2, L9_2, L10_2, L11_2
-  L1_2 = PlayUISound
-  L2_2 = UISoundEventUnlock_Asset
-  L1_2(L2_2)
-  L1_2 = PushMenu
-  L2_2 = "UnlockedPopup"
-  L1_2(L2_2)
-  L1_2 = UnlockedPopup
-  L2_2 = L1_2
-  L1_2 = L1_2.UnlockedItem
-  L3_2 = "Characters"
-  L4_2 = ""
-  L5_2 = GetLocalizedString
-  L6_2 = "ingameui"
-  L7_2 = "STR_GLOBAL_UNLOCKS_4"
-  L5_2 = L5_2(L6_2, L7_2)
-  L6_2 = " "
-  L7_2 = GetLocalizedString
-  L8_2 = "CommonUI"
-  L9_2 = "STR_"
-  L10_2 = A0_2
-  L11_2 = "_NAME"
-  L9_2 = L9_2 .. L10_2 .. L11_2
-  L7_2 = L7_2(L8_2, L9_2)
-  L4_2 = L4_2 .. L5_2 .. L6_2 .. L7_2
-  L1_2(L2_2, L3_2, L4_2)
-  L1_2 = RaiseMenu
-  L2_2 = "UnlockedPopup"
-  L1_2(L2_2)
-end
-L0_1.onUnlockedVehicle = L1_1
-VehicleDatabase = L0_1
+}
+CustomParts = {
+  onUnlockedPart = function(_ARG_0_)
+    PlayUISound(UISoundEventUnlock_Asset)
+    PushMenu("UnlockedPopup")
+    UnlockedPopup:UnlockedItem("Parts", "" .. GetLocalizedString("ingameui", "STR_GLOBAL_UNLOCKS_5") .. " " .. GetLocalizedString("CommonUI", "STR_MCQ_NAME"))
+    RaiseMenu("UnlockedPopup")
+  end
+}
+PaintJobs = {
+  onCollected = function(_ARG_0_)
+    PlayUISound(UISoundEventUnlock_Asset)
+    PushMenu("UnlockedPopup")
+    UnlockedPopup:UnlockedItem("PaintJobs", "" .. GetLocalizedString("ingameui", "STR_GLOBAL_UNLOCKS_2") .. " " .. GetLocalizedString("CommonUI", _ARG_0_))
+    RaiseMenu("UnlockedPopup")
+  end
+}
+WheelSets = {
+  onCollected = function(_ARG_0_)
+    PlayUISound(UISoundEventUnlock_Asset)
+    PushMenu("UnlockedPopup")
+    UnlockedPopup:UnlockedItem("WheelSets", "" .. GetLocalizedString("ingameui", "STR_GLOBAL_UNLOCKS_1"))
+    RaiseMenu("UnlockedPopup")
+  end
+}
+VehicleDatabase = {
+  onUnlockedVehicle = function(_ARG_0_)
+    PlayUISound(UISoundEventUnlock_Asset)
+    PushMenu("UnlockedPopup")
+    UnlockedPopup:UnlockedItem("Characters", "" .. GetLocalizedString("ingameui", "STR_GLOBAL_UNLOCKS_4") .. " " .. GetLocalizedString("CommonUI", "STR_" .. _ARG_0_ .. "_NAME"))
+    RaiseMenu("UnlockedPopup")
+  end
+}
