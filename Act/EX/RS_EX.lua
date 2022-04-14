8fa7758dcb22d1e5994f75b4ccee5034135469b1
Jordon87 <38020644+Jordon87@users.noreply.github.com>
2022-04-14 12:00:51 +1200

    Add files via upload

---
+++ Act/EX/RS_EX.lua
@@ -0,0 +1,23 @@
+RS_EX = {
+  ChangedState = function(_ARG_0_, _ARG_1_)
+    if _ARG_1_ == State_PreEvent then
+      DebugPrint("preevent")
+    elseif _ARG_1_ == State_PreEventUI then
+      DebugPrint("preeventUI")
+      SetHUD("ExploreHUD")
+    elseif _ARG_1_ == State_Begin then
+      DebugPrint("begin")
+    elseif _ARG_1_ == State_Rollup then
+      DebugPrint("rollup")
+    elseif _ARG_1_ == State_Countdown then
+      DebugPrint("countdown")
+    elseif _ARG_1_ == State_Running then
+      DebugPrint("running")
+    elseif _ARG_1_ == State_GameOver then
+      DebugPrint("gameover")
+    elseif _ARG_1_ == State_End then
+    elseif _ARG_1_ == State_PostEvent then
+    elseif _ARG_1_ == State_PostEventUI then
+    end
+  end
+}
