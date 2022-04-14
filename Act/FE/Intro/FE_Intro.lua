8ef678f6aeda03b0426e3373c9c224385cc6d3d8
Jordon87 <38020644+Jordon87@users.noreply.github.com>
2022-04-14 12:04:50 +1200

    Add files via upload

--- Act/FE/Intro/FE_Intro.lua
+++ Act/FE/Intro/FE_Intro.lua
@@ -1,109 +1,35 @@
-local L0_1, L1_1
-L0_1 = {}
-function L1_1(A0_2, A1_2)
-  local L2_2, L3_2, L4_2
-  L2_2 = State_PreEvent
-  if A1_2 == L2_2 then
-    L2_2 = DebugPrint
-    L3_2 = "preevent"
-    L2_2(L3_2)
-    L2_2 = GetPlatform
-    L2_2 = L2_2()
-    if L2_2 ~= "x360" then
-      L2_2 = GetPlatform
-      L2_2 = L2_2()
-      if L2_2 ~= "ps3" then
-        goto lbl_17
-      end
-    end
-    A0_2.srcMovieDir = "C\\Movies\\720\\"
-    goto lbl_24
-    ::lbl_17::
-    L2_2 = IsDisplayModePAL
-    L2_2 = L2_2()
-    if L2_2 then
-      A0_2.srcMovieDir = "C\\Movies\\2500\\"
-    else
-      A0_2.srcMovieDir = "C\\Movies\\2997\\"
-    end
-    ::lbl_24::
-    L2_2 = QueueVideo
-    L3_2 = "Disney"
-    L4_2 = A0_2.srcMovieDir
-    L2_2(L3_2, L4_2)
-    L2_2 = QueueVideo
-    L3_2 = "Pixar"
-    L4_2 = A0_2.srcMovieDir
-    L2_2(L3_2, L4_2)
-    L2_2 = QueueVideo
-    L3_2 = "THQ"
-    L4_2 = A0_2.srcMovieDir
-    L2_2(L3_2, L4_2)
-    L2_2 = QueueVideo
-    L3_2 = "Incin"
-    L4_2 = A0_2.srcMovieDir
-    L2_2(L3_2, L4_2)
-    L2_2 = StartVideoQueue
-    L2_2()
-  else
-    L2_2 = State_PreEventUI
-    if A1_2 == L2_2 then
-      L2_2 = DebugPrint
-      L3_2 = "preeventUI"
-      L2_2(L3_2)
-    else
-      L2_2 = State_Begin
-      if A1_2 == L2_2 then
-        L2_2 = DebugPrint
-        L3_2 = "begin"
-        L2_2(L3_2)
+FE_Intro = {
+  ChangedState = function(_ARG_0_, _ARG_1_)
+    if _ARG_1_ == State_PreEvent then
+      DebugPrint("preevent")
+      if GetPlatform() == "x360" or GetPlatform() == "ps3" then
+        _ARG_0_.srcMovieDir = "C\\Movies\\720\\"
+      elseif IsDisplayModePAL() then
+        _ARG_0_.srcMovieDir = "C\\Movies\\2500\\"
       else
-        L2_2 = State_Rollup
-        if A1_2 == L2_2 then
-          L2_2 = DebugPrint
-          L3_2 = "rollup"
-          L2_2(L3_2)
-        else
-          L2_2 = State_Countdown
-          if A1_2 == L2_2 then
-            L2_2 = DebugPrint
-            L3_2 = "countdown"
-            L2_2(L3_2)
-          else
-            L2_2 = State_Running
-            if A1_2 == L2_2 then
-              L2_2 = DebugPrint
-              L3_2 = "running"
-              L2_2(L3_2)
-            else
-              L2_2 = State_GameOver
-              if A1_2 == L2_2 then
-                L2_2 = DebugPrint
-                L3_2 = "gameover"
-                L2_2(L3_2)
-              else
-                L2_2 = State_End
-                if A1_2 == L2_2 then
-                  L2_2 = SwitchActivity
-                  L3_2 = "FE_FrontEnd"
-                  L4_2 = "player1start"
-                  L2_2(L3_2, L4_2)
-                else
-                  L2_2 = State_PostEvent
-                  if A1_2 == L2_2 then
-                  else
-                    L2_2 = State_PostEventUI
-                    if A1_2 == L2_2 then
-                    end
-                  end
-                end
-              end
-            end
-          end
-        end
+        _ARG_0_.srcMovieDir = "C\\Movies\\2997\\"
       end
+      QueueVideo("Disney", _ARG_0_.srcMovieDir)
+      QueueVideo("Pixar", _ARG_0_.srcMovieDir)
+      QueueVideo("THQ", _ARG_0_.srcMovieDir)
+      QueueVideo("Incin", _ARG_0_.srcMovieDir)
+      StartVideoQueue()
+    elseif _ARG_1_ == State_PreEventUI then
+      DebugPrint("preeventUI")
+    elseif _ARG_1_ == State_Begin then
+      DebugPrint("begin")
+    elseif _ARG_1_ == State_Rollup then
+      DebugPrint("rollup")
+    elseif _ARG_1_ == State_Countdown then
+      DebugPrint("countdown")
+    elseif _ARG_1_ == State_Running then
+      DebugPrint("running")
+    elseif _ARG_1_ == State_GameOver then
+      DebugPrint("gameover")
+    elseif _ARG_1_ == State_End then
+      SwitchActivity("FE_FrontEnd", "player1start")
+    elseif _ARG_1_ == State_PostEvent then
+    elseif _ARG_1_ == State_PostEventUI then
     end
   end
-end
-L0_1.ChangedState = L1_1
-FE_Intro = L0_1
+}
